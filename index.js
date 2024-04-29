d97526a8f4c1b0591dc7326b5288082433e0d893
AgentShini <mayowaakande73@gmail.com>
2024-04-29 13:03:07 +0100

    Added .env file

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+
 const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
@@ -7,7 +11,7 @@ var getAllAddresses = require("./routes/Get_All_Address");
 var userLocation = require("./routes/User_Location");
 var riderDistance = require("./routes/Rider_Distance");
 
- mongoose.connect("mongodb://localhost:27017/locations");
+ mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.zeh2plr.mongodb.net/`);
  mongoose.connection.on('connected', (err) => {
   if (err) {
       console.log(err);
@@ -21,6 +25,6 @@ app.use("/api",getAllAddresses,userLocation,riderDistance);
 
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running`);
 });
 
